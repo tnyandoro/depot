b9b8a04e624b6e92ae4a909837862311c4a213c5
tnyandoro <tnyandoro@gmail.com>
2023-06-05 22:53:34 +0200

    Add the seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.delete_all
+# . . .
+Product.create!(
+  title: 'Design and Build Great Web APIs',
+  description: "<p>
+        <em>Robust, Reliable, and Resilient </em>
+        APIs are transforming the business world at an increasing pace. Gain
+        the essential skills needed to quickly design, build, and deploy
+        quality web APIs that are robust, reliable, and resilient. Go from
+        initial design through prototyping and implementation to deployment of
+        mission-critical APIs for your organization. Test, secure, and deploy
+        your API with confidence and avoid the “release into production”
+        panic. Tackle just about any API challenge with more than a dozen
+        open-source utilities and common programming patterns you can apply
+        right away.
+      </p>",
+  image_url: 'maapis.jpg',
+  price: 24.95
+)
