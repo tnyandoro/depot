0f73e7597b86a4d3c539272215da4570256829b6
tnyandoro <tnyandoro@gmail.com>
2023-06-07 05:44:09 +0200

    Add pricing tests

--- app/models/product_test.rb
+++ app/models/product_test.rb
@@ -1,12 +1,30 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  test "product attribute must not be empty" do
+  test 'product attribute must not be empty' do
     product = Product.new
     assert product.invalid?
     assert product.errors[:title].any?
     assert product.errors[:description].any?
     assert product.errors[:price].any?
     assert product.errors[:image_url].any?
+  end
+
+  test 'product price must be positive' do
+    product = Product.new(title: 'Book title',
+                          description: 'abcd',
+                          image_url: 'def.jpg')
+    product.price = -1
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'],
+      product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'],
+      product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
   end
 end
