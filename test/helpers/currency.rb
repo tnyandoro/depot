d6340e2c0b3f3e1b15edf06badb8224445bea4f3
Tendai <tendai1@hotmail.com>
2023-06-09 13:46:59 +0200

    Revert "Task c"

--- test/helpers/currency.rb
+++
@@ -1,5 +0,0 @@
-def convert_to_usd(amount_in_zar)
-  exchange_rate = 0.072 # Example exchange rate: 1 ZAR = 0.072 USD
-  converted_amount = amount_in_zar * exchange_rate
-  converted_amount.round(2)
-end
