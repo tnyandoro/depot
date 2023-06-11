77cb0cea22ee0e00de178d4dfbce5474e17d39ab
tnyandoro <tnyandoro@gmail.com>
2023-06-11 19:05:40 +0200

    Error handling

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -6,7 +6,7 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select 'nav a', minimum: 4
     assert_select 'main ul li', 3
-    assert_select 'h2', 'Programming Ruby Ruby 1.9'
+    assert_select 'h2', 'Programming Ruby 1.9'
     assert_select 'div', /\$[,\d]+\.\d/
   end
 end
